df29c29c40adb0e927b31eb90d323bc7810a3607
amirb <amir.bullock9@gmail.com>
2023-06-01 22:12:01 -0400

    implement sign up functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
