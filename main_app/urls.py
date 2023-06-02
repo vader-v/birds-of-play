710c15d9a9f58e2b6049fb202b21cd6980dc8feb
amirb <amir.bullock9@gmail.com>
2023-06-02 14:13:04 -0400

    add bird index view

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('accounts/signup/', views.signup, name='signup'),
+  path('birds/', views.bird_index, name='bird-index'),
 ]
