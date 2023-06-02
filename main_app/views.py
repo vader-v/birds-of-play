ddf20e617090ad819dc76ffe69f0d64bea76473f
amirb <amir.bullock9@gmail.com>
2023-06-02 13:55:59 -0400

    add bird_index view function

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .models import Bird
 
 class Home(LoginView):
   template_name = 'home.html'
@@ -9,22 +10,20 @@ class Home(LoginView):
 def about(request):
   return render(request, 'about.html')
 
+def bird_index(request):
+  birds = Bird.objects.all()
+  return render(request, 'birds/index.html', { 'birds': birds })
+
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in
       login(request, user)
       return redirect('home')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'signup.html', context)
-  # Same as: return render(request, 'signup.html', {'form': form, 'error_message': error_message})
