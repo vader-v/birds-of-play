3bac2aec4ae8c53ae3e4446338366d04a33f7e3b
amirb <amir.bullock9@gmail.com>
2023-06-02 13:25:43 -0400

    add Bird model and migrate

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Bird(models.Model):
+  name = models.CharField(max_length=200)
+  origin = models.CharField(max_length=200)
+  description = models.CharField(max_length=200)
+  times_seen = models.IntegerField()
+  
+  def __str__(self):
+      return self.name
