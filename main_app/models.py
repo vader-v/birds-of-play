16e582924c50081fc41d9de901501f3e87708aea
amirb <amir.bullock9@gmail.com>
2023-06-03 15:01:18 -0400

    add photo model

--- main_app/models.py
+++ main_app/models.py
@@ -8,10 +8,16 @@ class Bird(models.Model):
   description = models.CharField(max_length=200)
   times_seen = models.IntegerField()
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  
+
   def __str__(self):
       return self.name
   
   def get_absolute_url(self):
       return reverse("bird-detail", kwargs={"bird-id": self.id})
-  
+
+class Photo(models.Model):
+  url = models.CharField(max_length=250)
+  bird = models.OneToOneField(Bird, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for bird_id: {self.bird_id} @{self.url}"
