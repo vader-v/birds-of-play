331284f89b50c75fcaeb2a9931ab1e982817d4ff
amirb <amir.bullock9@gmail.com>
2023-06-03 13:00:59 -0400

    add User field to Bird model and migrate

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Bird(models.Model):
   name = models.CharField(max_length=200)
   origin = models.CharField(max_length=200)
   description = models.CharField(max_length=200)
   times_seen = models.IntegerField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   
   def __str__(self):
       return self.name
