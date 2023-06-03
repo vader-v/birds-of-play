1d8142149578399eb925ae963a786a3cd31bf447
amirb <amir.bullock9@gmail.com>
2023-06-03 09:54:59 -0400

    add template and view function for bird create

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('accounts/signup/', views.signup, name='signup'),
   path('birds/', views.bird_index, name='bird-index'),
+  path('birds/create/', views.BirdCreate.as_view(), name='bird-create'),
 ]
